80abcb0f7c042af53b1e5852ab416388f13a2a15
KHAV <khweld454@gmail.com>
2018-07-17 00:56:00 +0300

    Create Saddle.php

---
+++ src/StackerPE/xkhhv/item/Saddle.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace StackerPE\xkhhv\item;
+
+use pocketmine\item\Item;
+
+class Saddle extends Item {
+
+    public function __construct(int $meta = 0) {
+        parent::__construct(self::SADDLE, $meta, "Saddle");
+    }
+
+}
